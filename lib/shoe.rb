b578e3ce9fb95043c06e6b87b3be7961531317e7
Pharia Le <pharia.le@gmail.com>
2020-03-11 20:02:00 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,26 @@
+require 'pry'
+
 class Shoe
+  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  
+  BRANDS = []
+  
   def initialize(brand)
+    self.brand=(brand)
     @brand = brand
+    # can remove self.brand=(brand) & def brand=(brand) if we add BRANDS << BRAND here. Otherwise method brand= is not being invoked
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
+  end
+  
 end
