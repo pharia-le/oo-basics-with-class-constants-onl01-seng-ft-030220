dd7db51f1d7c7736cda5381136c5a60f42c05830
Pharia Le <pharia.le@gmail.com>
2020-03-11 19:11:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,10 @@
 class Shoe
+  
+  BRANDS = []
+  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  
   def initialize(brand)
     @brand = brand
   end
@@ -10,5 +13,10 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
+  
 end
